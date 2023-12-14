94a51d8490ea3fb33538936619c726740ebaaf7a
h4st27 <a.o.mikhalchenko@gmail.com>
2023-12-14 17:29:52 +0200

    lr10-11

--- App.js
+++ App.js
@@ -1,68 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Onboarding from "./components/Onboarding";
-import Greetings from "./components/Greetings";
-import { useState } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import OrganizerApp from "./components/OrganizerApp";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import Click from "./components/Click";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import { Hello } from './components/Hello';
 
 export default function App() {
-  const [animationFinished, setAnimationFinished] = useState(false);
-  const Tab = createBottomTabNavigator();
   return (
-    <>
-      {animationFinished ? (
-        <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen
-              options={{
-                tabBarIcon: () => (
-                  <MaterialCommunityIcons name="home" color="#000" size={30} />
-                ),
-              }}
-              name="Home"
-              component={Onboarding}
-            />
-            <Tab.Screen
-              options={{
-                tabBarIcon: () => (
-                  <MaterialCommunityIcons name="seal" color="#000" size={30} />
-                ),
-              }}
-              name="OrganizerApp"
-              component={OrganizerApp}
-            />
-            <Tab.Screen
-              options={{
-                tabBarIcon: () => (
-                  <MaterialCommunityIcons
-                    name="clippy"
-                    color="#000"
-                    size={30}
-                  />
-                ),
-              }}
-              name="Click"
-              component={Click}
-            />
-          </Tab.Navigator>
-        </NavigationContainer>
-      ) : (
-        <Greetings onAnimationFinish={() => setAnimationFinished(true)} />
-      )}
-      <StatusBar style="auto" />
-    </>
+    <Provider store={store} >
+      <Hello />
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
