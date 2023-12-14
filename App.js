df405c2c0365019ca4bdfe2ac5eb36976a9edacb
h4st27 <a.o.mikhalchenko@gmail.com>
2023-12-14 16:19:34 +0200

    lr8

--- App.js
+++ App.js
@@ -1,96 +1,28 @@
-import React, { useState } from "react";
-import { StyleSheet, AppRegistry, Text } from "react-native";
-import LoginForm from "./components/LoginForm/LoginForm";
-import {
-  NativeBaseProvider,
-  View,
-  Center,
-  Button,
-  PresenceTransition,
-} from "native-base";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Onboarding from "./components/Onboarding";
+import Greetings from "./components/Greetings";
+import { useState } from "react";
 
 export default function App() {
-  const [password, setPassword] = useState("password");
-  const [login, setLogin] = useState("login");
-  const [isCorrect, setIsCorrect] = useState(null);
-  const handleLogin = () => {
-    users.some((val) => val.password == password && val.login == login)
-      ? setIsCorrect(true)
-      : setIsCorrect(false);
-  };
-  const users = [
-    {
-      login: "sanya@gmail.com",
-      password: "12345678",
-    },
-  ];
+  const [animationFinished, setAnimationFinished] = useState(false);
   return (
-    <NativeBaseProvider>
-      <PresenceTransition
-        visible={isCorrect != null}
-        initial={{
-          translateY: -100,
-          opacity: 0,
-        }}
-        animate={{
-          translateY: 0,
-          opacity: 1,
-          transition: {
-            duration: 250,
-          },
-        }}
-      >
-        <Center
-          bg={!isCorrect ? "blue.400" : "green.500"}
-          rounded="md"
-          w="100%"
-          h="75"
-          _text={{
-            color: "white",
-          }}
-          textAlign={"center"}
-        >
-          {!isCorrect
-            ? "Invalid password or login, try again"
-            : "You are authorized"}
-        </Center>
-      </PresenceTransition>
-      <View style={styles.container}>
-        <View style={styles.inputsContainer}>
-          <LoginForm
-            inpData={login}
-            setInpData={setLogin}
-            type={"text"}
-            label="Login"
-            isCorrect={isCorrect}
-          />
-          <LoginForm
-            inpData={password}
-            setInpData={setPassword}
-            type={"password"}
-            label="Password"
-            isCorrect={isCorrect}
-          />
-        </View>
-        <Center style={styles.button}>
-          <Button onPress={() => handleLogin()} width={"100%"} backgroundColor={"black"}>
-            Login
-          </Button>
-        </Center>
-      </View>
-    </NativeBaseProvider>
+    <>
+      {animationFinished ? (
+					<Onboarding/>
+      ) : (
+        <Greetings onAnimationFinish={() => setAnimationFinished(true)} />
+      )}
+      <StatusBar style="auto" />
+    </>
   );
 }
 
-AppRegistry.registerComponent("MyApp", () => App);
-
 const styles = StyleSheet.create({
   container: {
-    margin: "auto",
-    marginTop: 250,
-    position: "relative",
-  },
-  inputsContainer: {
-    height: 520,
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
